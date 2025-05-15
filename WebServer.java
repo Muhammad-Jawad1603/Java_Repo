c89e5ddbd4f3e8c2a9049478269c10e0afeb7203
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-05-15 18:01:40 +0500

    changes

--- WebServer.java
+++ WebServer.java
@@ -6,7 +6,7 @@ import java.net.InetSocketAddress;
 
 public class WebServer {
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", new HelloHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
