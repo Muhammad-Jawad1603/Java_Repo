436a900c2c809386a839dffec263b31da90a337e
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-05-15 15:16:04 +0500

    changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cd /home/ubuntu/docker-app
-sudo docker run -d -p 80:8080 --name java-hello java-hello
+
+# Login to ECR
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 099199746132.dkr.ecr.eu-west-1.amazonaws.com
+
+# Pull latest image
+sudo docker pull 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo:latest
+
+# Run container
+sudo docker run -d -p 80:8080 --name java-hello 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo:latest
