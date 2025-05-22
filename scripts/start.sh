284f87b313f3b56baf9ac13ef16651d1c2ec5310
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-05-22 15:09:15 +0500

    new files

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu/java-hello
-sudo apt update
-sudo apt install -y docker-compose-plugin
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 099199746132.dkr.ecr.eu-west-1.amazonaws.com
-docker-compose pull
-docker-compose up -d --build
 
+# Login to ECR
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 099199746132.dkr.ecr.eu-west-1.amazonaws.com
+
+# Pull latest image & run
+docker compose pull
+docker compose up -d --build
