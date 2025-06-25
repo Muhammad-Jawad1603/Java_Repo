624afc4f3a2edea28c2b7df537d40be71e4a5f07
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-06-25 16:27:44 +0500

    Update WebServer.java

--- WebServer.java
+++ WebServer.java
@@ -16,7 +16,7 @@ public class WebServer {
     static class HelloHandler implements HttpHandler {
         public void handle(HttpExchange t) {
             try {
-                String response = "Hello, World from ECS!!!!!!!!!!!!!!!!!! Java !!!!!!!!!!!!!!!!!!!!!!!!! ECS!!!!!";
+                String response = "Hello, World from ECS!!";
                 t.sendResponseHeaders(200, response.length());
                 OutputStream os = t.getResponseBody();
                 os.write(response.getBytes());
